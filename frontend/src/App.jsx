bddf8f65f847ec62f3bfdc0626497172fb0e4482
vehidtr <vehid.trtak@gmail.com>
2023-11-01 15:24:09 +0100

    Improve UI structure and remove unused stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import routes from "utils/routes";
 import "App.scss";
+import Loading from "components/Loading/Loading";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <div className="app-content max-w-2xl mx-auto">
+      <div className="app-content max-w-2xl mx-auto">
+        <BrowserRouter>
           <Routes>
             {routes.map((route, i) => {
               const key = `route-${i}`;
@@ -17,16 +18,25 @@ function App() {
                   key={key}
                   path={route.path}
                   element={
-                    <Suspense fallback={<div>Loading</div>}>
-                      <route.component />
+                    <Suspense
+                      fallback={
+                        <div className="flex justify-center items-center w-full h-screen">
+                          <Loading />
+                          <p>Loading...</p>
+                        </div>
+                      }
+                    >
+                      <div className="flex flex-col relative justify-center items-center md:shadow-md md:my-8 bg-[lightseagreen] rounded-xl">
+                        <route.component />
+                      </div>
                     </Suspense>
                   }
                 />
               );
             })}
           </Routes>
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import logo from "assets/images/logo1.png";
 import plusBtn from "assets/images/plus-icon.svg";
 import closeBtn from "assets/images/close-icon.svg";
 import searchIcon from "assets/images/search-icon.svg";
+import synonymIcon from "assets/images/synonym-icon.png";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -27,85 +28,83 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col relative justify-center items-center md:shadow-md md:my-8 bg-[lightseagreen] md:rounded-xl">
-        <div
-          className="absolute z-10 top-0 right-0 bg-white rounded-full p-[18px] mt-5 cursor-pointer ml-auto mr-5 text-3xl text-orange-500 font-bold shadow-lg leading-5 md:slide-bl md:rotate-45"
-          onClick={() => navigate("/add")}
-        >
-          <img
-            src={plusBtn}
-            className="cursor-pointer leading-3 w-4 rotate-center"
-            alt=""
+      <div
+        className="absolute z-10 top-0 right-0 bg-white rounded-full p-[18px] mt-5 cursor-pointer ml-auto mr-5 text-3xl text-orange-500 font-bold shadow-lg leading-5 md:slide-bl md:rotate-45"
+        onClick={() => navigate("/add")}
+      >
+        <img
+          src={plusBtn}
+          className="cursor-pointer leading-3 w-4 rotate-center"
+          alt=""
+        />
+      </div>
+      <img className="w-80 h-80 md:slide-right" src={logo} alt="logo" />
+      <div className="flex gap-5 w-11/12 md:w-4/5 mx-auto">
+        <div className="flex relative items-center w-full">
+          <input
+            type="text"
+            placeholder="Find synonyms"
+            className="border rounded-md p-2 md:p-4 pl-2 text-xl w-full outline-none"
+            value={word || ""}
+            onChange={(e) => setWord(e.target.value)}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleSearchSynonyms();
+              }
+            }}
           />
-        </div>
-        <img className="w-80 h-80 md:slide-right" src={logo} alt="logo" />
-        <div className="flex gap-5 w-11/12 md:w-4/5 mx-auto">
-          <div className="flex relative items-center w-full">
-            <input
-              type="text"
-              placeholder="Find synonyms"
-              className="border rounded-md p-2 md:p-4 pl-2 text-xl w-full outline-none"
-              value={word || ""}
-              onChange={(e) => setWord(e.target.value)}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  handleSearchSynonyms();
-                }
-              }}
-            />
-            {word ? (
-              <img
-                src={closeBtn}
-                alt="close icon"
-                className="absolute w-5 right-0 mr-4 cursor-pointer"
-                onClick={handleClearWord}
-              />
-            ) : null}
-          </div>
-          <button
-            className="flex justify-center items-center bg-[coral] rounded-md px-3 py-1 text-white w-32 cursor-pointer text-xl hover:rounded-[40px] shadow-md transition-border duration-300 ease-in-out"
-            onClick={handleSearchSynonyms}
-          >
-            <p className="hidden md:block">Search</p>
+          {word ? (
             <img
-              className="block md:hidden w-6 h-6"
-              src={searchIcon}
-              alt="search icon"
+              src={closeBtn}
+              alt="close icon"
+              className="absolute w-5 right-0 mr-4 cursor-pointer"
+              onClick={handleClearWord}
             />
-          </button>
+          ) : null}
         </div>
-        <div className="w-11/12 md:w-4/5 flex text-white text-xs mt-1 italic">
-          Try typing strong or active
-        </div>
+        <button
+          className="flex justify-center items-center bg-[coral] rounded-md px-3 py-1 text-white w-32 cursor-pointer text-xl hover:rounded-[40px] shadow-md transition-border duration-300 ease-in-out"
+          onClick={handleSearchSynonyms}
+        >
+          <p className="hidden md:block">Search</p>
+          <img
+            className="block md:hidden w-6 h-6"
+            src={searchIcon}
+            alt="search icon"
+          />
+        </button>
+      </div>
+      <div className="w-11/12 md:w-4/5 flex text-white text-xs mt-1 italic">
+        Try typing strong or active
+      </div>
 
-        <div className="w-11/12 md:w-4/5  pb-10 mt-10">
-          <div className="mt-5 text-2xl text-white font-medium text-center">
-            SYNONYMS LIST
-          </div>
-          <ul className="flex flex-col mt-5 gap-4 mb-10">
-            {synonymsList?.length ? (
-              synonymsList.map((synon) => (
-                <li
-                  key={synon}
-                  className="flex items-center bg-white rounded-xl shadow-md lowercase first-letter:capitalize"
-                >
-                  <div className="rounded-l-xl bg-[#f2f2f2] p-3 mr-4">
-                    <img
-                      className="w-6 rotate-90 opacity-25"
-                      src="https://cdn-icons-png.flaticon.com/512/7382/7382017.png"
-                      alt="synonym"
-                    />
-                  </div>
-                  {synon}
-                </li>
-              ))
-            ) : (
-              <div className="text-center mt-10 text-white italic">
-                No synonyms to display...
-              </div>
-            )}
-          </ul>
+      <div className="w-11/12 md:w-4/5  pb-10 mt-10">
+        <div className="mt-5 text-2xl text-white font-medium text-center">
+          SYNONYMS LIST
         </div>
+        <ul className="flex flex-col mt-5 gap-4 mb-10">
+          {synonymsList?.length ? (
+            synonymsList.map((synon) => (
+              <li
+                key={synon}
+                className="flex items-center bg-white rounded-xl shadow-md lowercase first-letter:capitalize"
+              >
+                <div className="rounded-l-xl bg-[#f2f2f2] p-3 mr-4">
+                  <img
+                    className="w-6 rotate-90 opacity-25"
+                    src={synonymIcon}
+                    alt="synonym"
+                  />
+                </div>
+                {synon}
+              </li>
+            ))
+          ) : (
+            <div className="text-center mt-10 text-white italic">
+              No synonyms to display...
+            </div>
+          )}
+        </ul>
       </div>
     </>
   );
